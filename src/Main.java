7062e748b34a52add6bc6b0535f15e5f093654d3
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-10-14 11:42:47 +0200

    działania i zaokrąglania zrobione

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+
+        float x;
+        float y;
+        float suma;
+        float roznica;
+        float iloraz;
+        float iloczyn;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj liczbę x: ");
+        x = scanner.nextFloat();
+        System.out.println("Podaj liczbę y: ");
+        y = scanner.nextFloat();
+
+        suma = x + y;
+        roznica = x - y;
+        iloraz = x/y;
+        iloczyn = x * y;
+
+        System.out.println("Suma tych liczb to: ");
+        System.out.printf("%.2f", suma);
+        System.out.println("\n" + "Różnica tych liczb to: ");
+        System.out.printf("%.2f", roznica);
+        System.out.println("\n" + "Iloraz tych liczb to: ");
+        System.out.printf("%.2f", iloraz);
+        System.out.println("\n" + "Iloczyn tych liczb to: ");
+        System.out.printf("%.2f", iloczyn);
+
 
     }
 }
